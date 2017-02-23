d9f8b2d124ff8bc2915b8e49be8beb42b6b51b3b
Leonardojoao01 <ldrsjoao@inf.ufpel.edu.br>
2017-02-23 16:17:32 -0300

    Aula 8 quase finalizada

--- Aula_8/dtft_vf.m
+++ Aula_8/dtft_vf.m
@@ -10,13 +10,13 @@ function X = dtft_vf(x,n,M)
     end
     magX = abs(X); angX = angle(X);
     realX = real(X); imagX = imag(X);
-%     subplot(2,2,1); 
+     subplot(2,2,1); 
     plot(k/M,magX);grid
     xlabel('frequency in pi units'); title('Magnitude Part')
-%     subplot(2,2,3); plot(k/M,angX/pi);grid
-%     xlabel('frequency in pi units'); title('Angle Part')
-%     subplot(2,2,2); plot(k/M,realX);grid
-%     xlabel('frequency in pi units'); title('Real Part')
-%     subplot(2,2,4); plot(k/M,imagX);grid
-%     xlabel('frequency in pi units'); title('Imaginary Part')
+     subplot(2,2,3); plot(k/M,angX/pi);grid
+     xlabel('frequency in pi units'); title('Angle Part')
+     subplot(2,2,2); plot(k/M,realX);grid
+     xlabel('frequency in pi units'); title('Real Part')
+     subplot(2,2,4); plot(k/M,imagX);grid
+     xlabel('frequency in pi units'); title('Imaginary Part')
 end
